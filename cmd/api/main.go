8c9c3ae7a833d5e5dcccca2c7a9bbdfa3b328168
Marcel Vitor Junqui <163474051+VJunqui@users.noreply.github.com>
2026-04-06 12:40:23 -0300

    feat
    initial API setup with tasks endpoint (GET and POST)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/VJunqui/go-todo-api/internal/models"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "API rodando 🚀")
+var tasks []models.Task
+var nextID = 1
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	case http.MethodGet:
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(tasks)
+
+	case http.MethodPost:
+		var task models.Task
+
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil {
+			http.Error(w, "JSON inválido", http.StatusBadRequest)
+			return
+		}
+
+		task.ID = nextID
+		nextID++
+		task.Done = false
+
+		tasks = append(tasks, task)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(task)
+
+	default:
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/tasks", tasksHandler)
 
-	fmt.Println("Servidor rodando na porta 8080...")
+	log.Println("Servidor rodando na porta 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VJunqui/go-todo-api
+
+go 1.26.1
